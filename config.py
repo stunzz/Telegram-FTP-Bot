79e4d25a83f1d3ebaef54da18ef079c47ee409e8
Aditya Singh <99306842+stunzz@users.noreply.github.com>
2023-12-26 19:43:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 
 #API ID & API Hash -----> my.telegram.org
-api_id = 21874726 
-api_hash = '5e734e8fbc0dc811b2696d5754297835'
+api_id = 
+api_hash = ''
 
 #Bot Token -----> @BotFather
-token = '6846406692:AAEFwNDO1Mf5M3A6oUU-IfRyJjeyZrcvNck'
+token = ''
 
 #Session Name -----> optional
 session_name = 'FTP_Uploader'
 
 
 #The robot admin (the person who can give orders to the robot.) -----> @myidbot
-admins = [5215061139]
+admins = [0000000000,0000000000]
 
 # Chat id to send technical logs
-dev = -1002058769683
+dev = -000000000000
 
 # When a file is sent to the bot, first that file is downloaded from the Telegram repository and stored in the bot's server.
 # Here you need to specify its temporary storage path
@@ -28,7 +28,7 @@ ftp_path = ''
 
 # The files are temporarily downloaded after they are on the bot server. They are uploaded to another host through FTP.
 # Here we have to give FTP access to the bot.
-ftp_ip = 'ftp.adisingh.in'
-ftp_username = 'tgftpbot@adisingh.in'
-ftp_password = 'eB]++DGxPGEW'
-ftp_domain = 'https://adisingh.in/adisingh.in/tgftpbot/'
+ftp_ip = 'FTP Server IP or domain like ftp.xxx.com'
+ftp_username = ''
+ftp_password = ''
+ftp_domain = 'url to upload location'
